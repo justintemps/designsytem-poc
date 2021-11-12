985369f2799ed98b3e54adacefcfd785b485c1cf
justintemps <jesmith627@gmail.com>
2021-11-12 11:28:35 +0100

    Fix jsx files not loading

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,35 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/preset-create-react-app",
+  ],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [
+              [
+                require("@babel/preset-react").default,
+                { runtime: "automatic" },
+              ],
+              require("@babel/preset-env").default,
+            ],
+          },
+        },
+      ],
+    });
+
+    config.resolve.extensions.push(".js", ".jsx");
+
+    return config;
+  },
+};
