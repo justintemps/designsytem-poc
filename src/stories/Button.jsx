261a2a2eb541eac62506f0588c58ce4f4776786b
justintemps <jesmith627@gmail.com>
2021-11-25 17:59:04 +0100

    IDefault styles for button component

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button as Btn } from "@wfp/ui";
-import "./button.css";
+import "@wfp/styles/scss/components/button/_index.scss";
+import "./button.scss";
 
 export const Button = ({ children, ...props }) => {
-  return <Btn {...props}>{children}</Btn>;
+  return (
+    <Btn primary {...props}>
+      {children}
+    </Btn>
+  );
 };
 
 Button.propTypes = {
