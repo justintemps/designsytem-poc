aff6378c9afc83344b45f139f56cf1ce24f7b6b3
justintemps <jesmith627@gmail.com>
2022-01-06 10:36:50 +0100

    Update button styles

--- packages/react/src/components/Button/Button.jsx
+++ packages/react/src/components/Button/Button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button as UNButton } from "@un/react";
-import "./button.scss";
 
 const Button = ({ children, ...props }) => {
   return <UNButton {...props}>{children}</UNButton>;
