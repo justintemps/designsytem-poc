e10cbeb0e125eedaf5fc37daf8ad5d52f941c422
justintemps <jesmith627@gmail.com>
2022-01-07 14:50:47 +0100

    Add wfp theme with ilo prefixes

--- packages/react/src/stories/Button.stories.jsx
+++ packages/react/src/stories/Button.stories.jsx
@@ -7,11 +7,10 @@ export default {
   component: Button,
 };
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args}>Button</Button>;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
 Primary.args = {
   primary: true,
 };
